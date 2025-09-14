5f89c949c16d3f39427b54951a4d6575481b3ca2
minister1334 <minister1334@gmail.com>
2025-09-14 17:43:43 +0900

    add menu interaction

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ using namespace std;
 int dir[4][2]={{0,1},{0,-1},{1,0},{-1,0}};
 float angle=0.0;
 int width, height;
-float r=0.0,g=0.0,b=0.0;                //왼쪽
+float r=0.0,g=0.0,b=0.0;                 //왼쪽
 vector<vector<int>> board={{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                            { 0, 0, 0, 0, 0, 0,-1, 0,-1, 0},
                            { 0, 0, 0, 0, 0, 0,-1, 0,-1, 0},
@@ -30,6 +30,7 @@ int turn=1;
 int menu=0;
 float mx,my;
 int state=0;
+char s[100];
 /*
 state에 따른 상태
 -1: stage failed
@@ -39,6 +40,14 @@ state에 따른 상태
 */
 
 queue<tuple<int,int,int>> go;
+
+void renderstring(float x, float y, float z, char* str){
+    char *c;
+    glRasterPos3f(x, y, z);
+    for (c=str; *c != '\0'; c++){
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, *c);
+    }
+}
 
 void changesize(int w, int h) {
     if(h==0) h=1;
@@ -57,29 +66,6 @@ void changesize(int w, int h) {
     gluLookAt(0.0,0.0,5.0,
               0.0,0.0,0.0,
               0.0,1.0,0.0);
-}
-
-void stageclear(){
-    glColor3f(1.0,1.0,0.0);
-    glRasterPos3f(-0.3,0.0,0.1);
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,'c');
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,'l');
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,'e');
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,'a');
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,'r');
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,'!');
-}
-
-void stagefail(){
-    glColor3f(1.0,0.5,0.5);
-    glRasterPos3f(-0.3,0.0,0.1);
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,'f');
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,'a');
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,'i');
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,'l');
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,'e');
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,'d');
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,'!');
 }
 
 void menuopen(){
@@ -147,50 +133,6 @@ void sq(float x, float y, int color){
     glEnd();
 }
 
-void idle(void){
-    glColor3f(0.0,1.0,1.0);
-    glBegin(GL_QUADS);
-    glVertex3f(-1.0, -1.0, 0.0);
-    glVertex3f( 1.0, -1.0, 0.0);
-    glVertex3f( 1.0,  1.0, 0.0);
-    glVertex3f(-1.0,  1.0, 0.0);
-    glEnd();
-}
-
-void renderscene(void) {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    glPushMatrix();
-
-    glColor3f(0.0f,0.0f,0.0f);
-    glPointSize(5.0f);
-    glBegin(GL_POINTS);
-    glVertex3f(mx*4.0-2.0,2.0-my*4.0,1.2);
-    glEnd();
-
-    glColor3f(1.0,1.0,1.0);
-
-    if(state==1) stageclear();
-    if(state==-1) stagefail();
-    if(menu) menuopen();
-
-    for(int i=0;i<10;i++){
-        for(int j=0;j<10;j++){
-            if(board[p[0]][p[1]]==-2) state=1;
-            else if(board[p[0]][p[1]]>0&&board[p[0]][p[1]]<=turn) state=-1;
-            if(board[i][j]==-2) sq(-2.0+(i*scale),-2.0+(j*scale), 2);//goal
-            else if(board[i][j]==-1) sq(-2.0+(i*scale),-2.0+(j*scale), 4);//wall
-            else if(board[i][j]>0&&board[i][j]<=turn) sq(-2.0+(i*scale),-2.0+(j*scale), 1);//fire
-            else if(i==p[0] && j==p[1]) sq(-2.0+(i*scale),-2.0+(j*scale), 3);//player
-            else sq(-2.0+(i*scale),-2.0+(j*scale), 0);//space
-        }
-    }
-
-    glPopMatrix();
-
-    glutSwapBuffers();
-}
-
 void pnk(unsigned char key, int x, int y) {//눌린 키, 키가 눌렸을 때의 마우스 좌표
     switch(key){//게임이 끝나도 할 수 있는 행동들
     case 27:
@@ -238,9 +180,70 @@ void pnk(unsigned char key, int x, int y) {//눌린 키, 키가 눌렸을 때의 마우스 좌
 }
 
 void ctm(int button, int ud, int x, int y){//pressed button, up or down, x,y
-    if(button!=GLUT_LEFT_BUTTON || ud!=GLUT_DOWN) return;
-    mx=(float)x/width;
+    if(!(menu&&button==GLUT_LEFT_BUTTON&&ud==GLUT_DOWN)) return;
+    if(mx>0.435 && mx<0.565 && my>0.465 && my<0.535) {// reset
+        turn=1;
+        p[0]=0;
+        p[1]=9;
+        state=0;
+    }
+    else if(mx>0.435 && mx<0.565 && my>0.59 && my<0.66) {// exit
+        exit(0);
+    }
+}
+
+void mtm(int x, int y){
+    if(x<0||x>width||y<0||y>height) return;
+    if(width>height) x-=(width-height)/2;
+    mx=(float)x/height;
     my=(float)y/height;
+}
+
+void renderscene(void) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glPushMatrix();
+
+    glColor3f(0.0f,0.0f,0.0f);
+    glPointSize(5.0f);
+    glBegin(GL_POINTS);
+    glVertex3f((mx*4.0)-2.0,2.0-(my*4.0),0.2);
+    glEnd();
+
+    sprintf(s,"mx : %4.4f",mx);
+    renderstring(-1.9,-1.75,0.1,s);
+    sprintf(s,"my : %4.4f",my);
+    renderstring(-1.9,-1.9,0.1,s);
+
+    glColor3f(1.0,1.0,1.0);
+
+    if(state==1){//성공
+        glColor3f(1.0,1.0,0.0);
+        sprintf(s,"clear!");
+        renderstring(-0.3,0.0,0.1,s);
+    }
+    if(state==-1){//실패
+        glColor3f(1.0,0.5,0.5);
+        sprintf(s,"failed!");
+        renderstring(-0.3,0.0,0.1,s);
+    }
+    if(menu) menuopen();
+
+    for(int i=0;i<10;i++){
+        for(int j=0;j<10;j++){
+            if(board[p[0]][p[1]]==-2) state=1;
+            else if(board[p[0]][p[1]]>0&&board[p[0]][p[1]]<=turn) state=-1;
+            if(board[i][j]==-2) sq(-2.0+(i*scale),-2.0+(j*scale), 2);//goal
+            else if(board[i][j]==-1) sq(-2.0+(i*scale),-2.0+(j*scale), 4);//wall
+            else if(board[i][j]>0&&board[i][j]<=turn) sq(-2.0+(i*scale),-2.0+(j*scale), 1);//fire
+            else if(i==p[0] && j==p[1]) sq(-2.0+(i*scale),-2.0+(j*scale), 3);//player
+            else sq(-2.0+(i*scale),-2.0+(j*scale), 0);//space
+        }
+    }
+
+    glPopMatrix();
+
+    glutSwapBuffers();
 }
 
 int main(int argc, char **argv) {
@@ -276,6 +279,7 @@ int main(int argc, char **argv) {
     glutIdleFunc(renderscene);//idle 상태일 때 렌더링할 함수
 
     glutKeyboardFunc(pnk);
+    glutPassiveMotionFunc(mtm);
     glutMouseFunc(ctm);
 
     glutReshapeFunc(changesize);//창의 크기가 바뀌었을 때, 어떻게 할 것인가를 설정
